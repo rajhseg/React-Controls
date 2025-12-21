4d28e8c8f9f3c0e7604c613c99f96dec7c9117c7
Rajesh <rajhseg@gmail.com>
2025-12-21 09:47:03 +0530

    RDonutChart
    RDonutChart

--- React-Controls/src/App.jsx
+++ React-Controls/src/App.jsx
@@ -1,17 +1,17 @@
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
 import { RButton } from './RButton/RButton'
 import { RDonutChart, RDonutChartItem } from './RDonutChart/RDonutChart';
 
 function App() {
 
-  const handleClick = (e) => {
-    alert("hi");
-  }
+  const [chartItems, setChartItems] = useState([]);
 
-  
-  let donutItems = [];
+  const handleClick = (e) => {
+      let pieItem1 = new RDonutChartItem(24,'Batminton', 'darkgreen', 'white');
+      setChartItems((prev)=> [...prev, pieItem1]);
+  }
 
   const createDonutItems = () =>{
     let pieItem1 = new RDonutChartItem(24,'Cricket', 'grey', 'white');
@@ -20,13 +20,19 @@ function App() {
     let pieItem4 = new RDonutChartItem(44,'BaseBall', 'teal', 'white');
     let pieItem5 = new RDonutChartItem(14,'Hockey', 'darkblue', 'white');
     let pieItem6 = new RDonutChartItem(44,'Football', '#13297A', 'white');
+    
+    setChartItems((prev)=>[]);
 
+    let donutItems = [];
+    
     donutItems.push(pieItem1);
     donutItems.push(pieItem2);
     donutItems.push(pieItem3);
     donutItems.push(pieItem4);
     donutItems.push(pieItem5);
     donutItems.push(pieItem6);
+
+    setChartItems((prev)=> [...prev, ...donutItems]);
   }
 
   useEffect(()=>{
@@ -37,9 +43,7 @@ function App() {
       <>
         <RButton IsDisabled={false} onClick={handleClick}>Submit</RButton>
 
-        <RDonutChart DataListHeight={100} ChartWidth={300} ShadowColor={'blue'} ChartItems={donutItems} Opacity={'0.8'}></RDonutChart>
-
-        <RDonutChart ChartItems={[]}></RDonutChart>
+        <RDonutChart DataListHeight={100} ChartWidth={300} ShadowColor={'blue'} ChartItems={chartItems} Opacity={'0.8'}></RDonutChart>
       </>
   )
 }
